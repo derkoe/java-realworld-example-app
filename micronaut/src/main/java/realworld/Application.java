cd35f58b0f0cd1a5deb49c13b74cd293a23b1fea
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-01-10 11:25:08 +0100

    WIP

---
+++ micronaut/src/main/java/realworld/Application.java
@@ -0,0 +1,12 @@
+package realworld;
+
+import io.micronaut.runtime.Micronaut;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    Micronaut.run(Application.class, args);
+  }
+}
