8e6384744904492d246ae8c2332aff79cbb9e368
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-01-10 13:51:29 +0100

    feat: use PostgreSQL for "production"

--- base/src/main/resources/db/migration/V1__initial.sql
+++ base/src/main/resources/db/migration/V1__initial.sql
@@ -1,6 +1,6 @@
 create table users
 (
-    id binary (16) not null,
+    id uuid not null,
     bio      varchar(255),
     email    varchar(255),
     image    varchar(255),
@@ -10,8 +10,8 @@ create table users
 );
 create table follows
 (
-    user_id binary (16) not null,
-    follower_id binary (16) not null,
+    user_id uuid not null,
+    follower_id uuid not null,
     primary key (user_id, follower_id),
     foreign key (user_id) references users (id),
     foreign key (follower_id) references users (id)
@@ -20,48 +20,48 @@ create table follows
 
 create table articles
 (
-    id binary (16) not null,
+    id uuid not null,
     body        varchar(255),
     created_at  timestamp,
     description varchar(255),
     slug        varchar(255),
     title       varchar(255),
     updated_at  timestamp,
-    author_id binary (16),
+    author_id uuid,
     primary key (id),
     foreign key (author_id) references users (id)
 );
 create table tags
 (
-    id binary (16) not null,
+    id uuid not null,
     name varchar(255),
     primary key (id)
 );
 create table articles_tags
 (
-    article_id binary (16) not null,
-    tags_id binary (16) not null,
+    article_id uuid not null,
+    tags_id uuid not null,
     primary key (article_id, tags_id),
     foreign key (article_id) references articles (id),
     foreign key (tags_id) references tags (id)
 );
 create table comments
 (
-    id binary (16) not null,
-    article_id binary (16),
+    id uuid not null,
+    article_id uuid,
     body       varchar(255),
     created_at timestamp,
     updated_at timestamp,
-    user_id binary (16),
+    user_id uuid,
     primary key (id),
     foreign key (article_id) references articles(id),
     foreign key (user_id) references users(id)
 );
 create table articles_favorites
 (
-    id binary (16) not null,
-    user_id binary (16),
-    article_id binary (16),
+    id uuid not null,
+    user_id uuid,
+    article_id uuid,
     primary key (id),
     foreign key (user_id) references users(id),
     foreign key (article_id) references articles(id)
